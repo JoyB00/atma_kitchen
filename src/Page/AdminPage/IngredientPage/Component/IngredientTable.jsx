790733b69150c6e391919a69b3026d5e2fc3917c
JoyB00 <dendypramartha7e@gmail.com>
2024-05-05 00:20:10 +0700

    edit ingredient

--- src/Page/AdminPage/IngredientPage/Component/IngredientTable.jsx
+++ src/Page/AdminPage/IngredientPage/Component/IngredientTable.jsx
@@ -13,7 +13,7 @@ import toast from "react-hot-toast";
 import { useAtom } from "jotai";
 import { loadEdit } from "../../HampersPage/HampersPage";
 import { BeatLoader } from "react-spinners";
-export default function IngredientTable({ search, data }) {
+export default function IngredientTable({ search, data, length }) {
   const [page, setPage] = useState(1);
   const ingredientPerPage = 8;
   const startIndex = (page - 1) * ingredientPerPage;
@@ -86,82 +86,82 @@ export default function IngredientTable({ search, data }) {
 
   return (
     <>
-    
-    
-    <table className=" w-full mt-4 mb-6  text-gray-500 bg-white rounded-2xl drop-shadow-md">
-      
-      <thead className="border-b-2">
-        <tr>
-          <th className="text-start font-medium py-8 ps-8">Ingredient Name</th>
-          <th className="text-start font-medium pe-6 ">Qty</th>
-          <th className="text-start font-medium pe-6">Unit</th>
-          <th className="text-center font-medium pe-6">Action
-          </th>
-        </tr>
-      </thead>
-      <motion.tbody
+      <table className=" w-full mt-4 mb-6  text-gray-500 bg-white rounded-2xl drop-shadow-md">
+        <thead className="border-b-2">
+          <tr>
+            <th className="text-start font-medium py-8 ps-8">
+              Ingredient Name
+            </th>
+            <th className="text-start font-medium pe-6 ">Qty</th>
+            <th className="text-start font-medium pe-6">Unit</th>
+            <th className="text-center font-medium pe-6">Action</th>
+          </tr>
+        </thead>
+        <motion.tbody
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-      >
-        {data
-          .filter((item) => {
-            return search.toLowerCase() === ""
-              ? item
-              : item.ingredient_name.toLowerCase().includes(search) ||
-                  item.ingredient_name.includes(search);
-          })
-          .slice(startIndex, endIndex)
-          .map((item) => (
-            <motion.tr
-              variants={ingredientItem}
-              className="border-t-2 border-gray-100  text-black"
-              key={item.id}
-            >
-               <td className="ps-3 text-[1rem]">{item.ingredient_name}</td>
-              <td className="font-medium text-start">{item.quantity}</td>
-              <td className="font-medium text-start">{item.unit}</td>
-              <td className="font-medium ">
-              <div className="flex justify-center me-2">
-                  <NavLink to={`/AdminDashboard/ingredient/${item.id}`}>
+        >
+          {data
+            .filter((item) => {
+              return search.toLowerCase() === ""
+                ? item
+                : item.ingredient_name.toLowerCase().includes(search) ||
+                    item.ingredient_name.includes(search);
+            })
+            .slice(startIndex, endIndex)
+            .map((item) => (
+              <motion.tr
+                variants={ingredientItem}
+                className="border-t-2 border-gray-100  text-black"
+                key={item.id}
+              >
+                <td className="ps-8 text-[1rem] py-6">
+                  {item.ingredient_name}
+                </td>
+                <td className="font-medium text-start">{item.quantity}</td>
+                <td className="font-medium text-start">{item.unit}</td>
+                <td className="font-medium ">
+                  <div className="flex justify-center me-2">
+                    <NavLink to={`/AdminDashboard/ingredient/${item.id}`}>
+                      <Button
+                        className="bg-orange-500 text-white me-2 px-4 text-[0.9rem]"
+                        onClick={() => handleLoadEdit(item.id)}
+                      >
+                        {load && idItemLoad == item.id ? (
+                          <BeatLoader color="white" loading={load} size={10} />
+                        ) : (
+                          <>
+                            <FontAwesomeIcon icon={faPencil} className="me-2" />
+                            Edit
+                          </>
+                        )}
+                      </Button>
+                    </NavLink>
                     <Button
-                      className="bg-orange-500 text-white me-2 px-4 text-[0.9rem]"
-                      onClick={() => handleLoadEdit(item.id)}
+                      className="bg-transparent border-orange-500 text-orange-500 hover:text-white px-2 text-[0.9rem]"
+                      onClick={() => swalDelete(item)}
                     >
-                      {load && idItemLoad == item.id ? (
-                        <BeatLoader color="white" loading={load} size={10} />
-                      ) : (
-                        <>
-                          <FontAwesomeIcon icon={faPencil} className="me-2" />
-                          Edit
-                        </>
-                      )}
+                      <FontAwesomeIcon icon={faTrash} className="me-2" />
+                      Delete
                     </Button>
-                  </NavLink>
-                  <Button
-                    className="bg-transparent border-orange-500 text-orange-500 hover:text-white px-2 text-[0.9rem]"
-                    onClick={() => swalDelete(item)}
-                  >
-                    <FontAwesomeIcon icon={faTrash} className="me-2" />
-                    Delete
-                  </Button>
-                </div>
-              </td>
-            </motion.tr>
-          ))}
-      </motion.tbody>
-      <tfoot>
-        <tr>
-          <td colSpan={5}>
-            <Pagination
-              count={Math.ceil(20 / ingredientPerPage)}
-              size="small"
-              className="flex justify-center mb-4"
-              onChange={handleChange}
-            />
-          </td>
-        </tr>
-      </tfoot>
+                  </div>
+                </td>
+              </motion.tr>
+            ))}
+        </motion.tbody>
+        <tfoot>
+          <tr>
+            <td colSpan={5}>
+              <Pagination
+                count={Math.ceil(length / ingredientPerPage)}
+                size="small"
+                className="flex justify-center mb-4"
+                onChange={handleChange}
+              />
+            </td>
+          </tr>
+        </tfoot>
       </table>
     </>
   );
