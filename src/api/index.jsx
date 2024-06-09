58d510ecc11a5aefd3c52bfd813d8c3423dc9e71
bootloopmaster636 <christhartono@hotmail.com>
2024-06-09 12:11:13 +0700

    fix: change api to cloud instead of local

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://127.0.0.1:8000";
+export const BASE_URL = "http://20.63.141.242:8000";
 
 export const getPicture = (picture, folder) => {
   return `${BASE_URL}/storage/${folder}/${picture}`;
